22b51f35575285994772c4af770603e7f78c9cbc
lazyonly4u <tanushreeshrestha@gmail.com>
2025-09-15 13:31:30 +0200

    Update Run.cs

--- textgame4.0/Methods/Run.cs
+++ textgame4.0/Methods/Run.cs
@@ -1,12 +1,21 @@
-﻿using System;
+using Methods;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace textgame4._0.Methods
+namespace Methods
 {
-    internal class Run
+    public class run
     {
+        public static void RunAway()
+        {
+            Console.WriteLine("You ran away successfully, but you feel a bit embarrassed. You lose 2 coins for your cowardice(lmao).");
+            Player.Coins -= 2;
+            if (Player.Coins < 0) Player.Coins = 0; // Prevent negative coins
+            Console.WriteLine($"You now have {Player.Coins} coins (Go broke).");
+            Console.ReadLine();
+        }
     }
 }
