ed238b67bb24db69cbfd4395d561731152a53c34
lazyonly4u <tanushreeshrestha@gmail.com>
2025-09-15 13:31:12 +0200

    Update Room.cs

--- textgame4.0/Methods/Room.cs
+++ textgame4.0/Methods/Room.cs
@@ -1,12 +1,26 @@
-﻿using System;
+using Methods;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace textgame4._0.Methods
+namespace Methods
 {
-    internal class Room
+    public class Room
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Room(string Name, string Description)
+        {
+            this.Name = Name;
+            this.Description = Description;
+        }
+        public void Enter()
+        {
+            Console.Clear();
+            Console.WriteLine($"You enter: {Name}");
+            Console.WriteLine(Description);
+        }
     }
 }
