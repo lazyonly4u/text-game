c2d22779aa786c4cb3a2a84edcef9daa55039aae
lazyonly4u <tanushreeshrestha@gmail.com>
2025-09-15 13:32:43 +0200

    Update Program.cs

--- textgame4.0/Program.cs
+++ textgame4.0/Program.cs
@@ -1,4 +1,4 @@
-﻿
+
 
 using Methods;
 using System;
@@ -35,14 +35,21 @@ namespace textgame
 
             List<Room> rooms = new List<Room>
             {
-            new Room("The Beginning", "You find yourself in a dimly lit dungeon with stone walls and a single wooden door. Beside the door is a sign that says: Level 0 - The beginning"),
-            new Room("Fungus Among Us", "Beside the door is a sign that says: Level 1 - Fungus Among Us. You open the door and see a dungeon room almost masks itself as a forest. And suprisingly its filled a huge waking mushroom. Do you wish to fight? or hide away like a coward"),
-            new Room("Slime Schlime Time", "A door Suddenly appears with markings: Level 2 - Slime Schlime Time. Through the door was an artificial looking cave dungeon. And boom a Slime blob appears and not the good type either. Do you wish to run or fight?"),
-            new Room("Nestflix & Chill", "Will you be able to survive through this? A door not so suprisingly appears with markings: Level 3 - Nestflix & Chill. You open the door and see a fancy dungeon room with pillars and suprisingly a few twigs fall down. You look up and you find nests, some with a few eggs and some with GRIFFINS?!? They look at you glaringly and with bloodlust. But only the weakest giffin wishes to fight you (you are not worthy enough). Do you wish to run or fight?"),
-            new Room("Oops, You’re Drowning", "Try and survive this! This time a fancy roman door appears(Very shinny): Level 4 - Pool of “Oops, You’re Drowning. You open the door and see a huge pillared roman pool with entrance being the only thing as a boarder. As you walk through the door, it slams LOUDLY. You see a deep blue shadowy figure in the water, you squint in hopes of seeing more and the huge figure splashes out the water violently. Congrats you have encounter a huge sea serpent with a glare of death(think you woke it up with the door). Do you wish to run or fight?"),
-            new Room("The Dino-Den of Doom", "Let me introduce you to Level 5! From a fancy door to a basic wooden door(don't judge a book by it cover...we had to rush this) on the now basic door is a sign that says: Level 5 - The Dino-Den of Doom. You open the door and see a huge dungeon room almost masking itself as a forest with huge rocks bigger than you(yes i'm calling you short hehehe >D ). In the center is a huge nest with dundunduunnn a Basilisk(bet you didn't guess that). Do you wish to run or fight?"),
-            new Room("Rock ‘n’ Roar Hideout", "HOW ABOUT THIS!! A door made out a unfinished paper mache(That...was um... supposed to be done...). Beside the door is a paper sign that says: Level 6 - Rock ‘n’ Roar Hideout. You open the door as residue of undried glue and paper sticks on your palm. You see huge dungeon room almost masking itself as a cave with huge rocks bigger than you(yes I just copied and pasted whatcha gon do about it?). As you walk deeper you feel the atmosphere getting hotter and next thing you know right in front of you is a legandary Wyvern(hireing him was the second most expensive thing). Do you wish to run or fight?"),
-            new Room("The unknown", "I honestly have no words... But you definitely are NOT getting past this (And yes, I’m totally petty about it). Suddenly, a golden door materializes (the third most expensive thing in the dungeon) With letters encapsulated in diamond saying: Level 7 - The unknown. You open it to find a room overflowing with the most luxurious treasures you’ve ever heard of... or seen. At the center, a grand staircase of pure gold, draped in rich red carpet, leads up to a throne. And sitting on that throne is... what is THAT?! A... DITTO?!?! (Took almost all our budget) DUN DUN DUUUUNNNN! *dramatic sound effect* Do you dare to fight or run away screaming?"),
+                new Room("The Beginning", "You find yourself in a dimly lit dungeon with stone walls and a single wooden door. Beside the door is a sign that says: Level 0 - The beginning. As you scan the walls, you faintly notice some graffiti: 'MAGI was here' scrawled in glittery ink. Strange... almost magical. You brush it off and proceed."),
+
+                new Room("Fungus Among Us", "Beside the door is a sign that says: Level 1 - Fungus Among Us. You open the door and see a dungeon room almost masking itself as a forest. Surprisingly, it's filled with a huge waking mushroom. Do you wish to fight, or hide away like a coward? Something glints beneath a mushroom cap... the letters L0ST etched faintly on a blade half-buried in the moss. It hums with cursed energy."),
+
+                new Room("Slime Schlime Time", "A door suddenly appears with markings: Level 2 - Slime Schlime Time. Through the door is an artificial-looking cave dungeon. Boom—a Slime blob appears (and not the good type either). Do you wish to run or fight? Oddly enough, a faint trail of glowing slime spells out something on the wall: 'Use MAGI. Is someone trying to help you?"),
+
+                new Room("Nestflix & Chill", "Will you be able to survive through this? A door (not-so-surprisingly) appears with markings: Level 3 - Nestflix & Chill. You open it to find a fancy dungeon room with pillars, and surprisingly, a few twigs fall down. You look up—nests. Some with eggs, some with GRIFFINS?!? They glare at you, bloodthirsty. Only the weakest griffin wishes to fight (you are not worthy enough). A twig rolls to your feet with something scratched into it: Av4. What does that mean?"),
+
+                new Room("Oops, You’re Drowning", "Try and survive this! A fancy Roman door appears (Very shiny): Level 4 - Pool of “Oops, You’re Drowning.” You open it and see a huge pillared Roman pool, the entrance being the only border. As you walk through the door, it slams LOUDLY. You spot a deep blue, shadowy figure in the water... a SEA SERPENT. Before it attacks, you notice a mural above the pool. Three symbols: Av, a 4, and what looks like elemental runes... Could it mean 'Avatar powers'?"),
+
+                new Room("The Dino-Den of Doom", "Let me introduce you to Level 5! From a fancy door to a basic wooden one (don't judge a book by its cover... we had to rush this). On the now-basic door is a sign: Level 5 - The Dino-Den of Doom. You open it and see a huge dungeon room almost masking itself as a forest, with rocks bigger than you (yes, I’m calling you short, hehehe >D ). In the center: a nest with... a Basilisk?! On the floor nearby, etched in claw marks: 'Only L0ST souls survive.' Creepy."),
+
+                new Room("Rock ‘n’ Roar Hideout", "HOW ABOUT THIS!! A door made out of unfinished papier-mâché (That...was um... supposed to be done...). Beside the door is a paper sign that says: Level 6 - Rock ‘n’ Roar Hideout. You open the door as undried glue and paper sticks to your palm. You enter a cave-like dungeon with huge rocks (yes, I copied and pasted that—what you gon’ do?). You feel the heat rising. Suddenly: a legendary Wyvern! On the back wall, burned into the stone: 'MAGI’s flame shall scorch the skies.'"),
+
+                new Room("The Unknown", "I honestly have no words... But you're NOT getting past this (And yes, I’m totally petty about it). A golden door materializes (third most expensive thing in the dungeon). Letters in diamond: Level 7 - The Unknown. Inside: luxurious treasures, and at the center, a golden staircase with red carpet, leading to a throne. And sitting on it: WHAT IS THAT?! A... DITTO?! DUN DUN DUUUNNN! *dramatic sound effect* On the throne’s base, nearly rubbed off: 'Av4 used here... once'. Do you dare to fight, or run away screaming?"),
             };
             bool[] levelsCompleted = new bool[rooms.Count]; // Default: all false
 
@@ -56,14 +63,14 @@ namespace textgame
 
             Weapons WoodenSword = new Weapons("Wooden Sword", 0, 5, false, 10);
 
-            Weapons LongSlenderBlade = new Weapons("Long Slender Blade", 16, 15, false, 15);
-            Weapons FrenchThinPencilSword = new Weapons("French Thin Pencil Sword", 22, 20, false, 20);
-            Weapons ArabCurvedThinSword = new Weapons("Arab Curved Thin Sword", 30, 35, false, 35);
+            Weapons LongSlenderBlade = new Weapons("Espada Bastarda", 16, 15, false, 15);
+            Weapons FrenchThinPencilSword = new Weapons("Rapier", 22, 20, false, 20);
+            Weapons ArabCurvedThinSword = new Weapons("Arabian scimitar sword", 30, 35, false, 35);
 
-            Weapons CurvedKnife = new Weapons("Curved Knife", 10, 10, false, 15);
+            Weapons CurvedKnife = new Weapons("Kukri", 10, 10, false, 15);
 
-            Weapons LongHorsebackBow = new Weapons("Long Horseback Bow", 13, 15, true, 15);
-            Weapons StrongBlowBow = new Weapons("Strong Blow Bow", 35, 35, true, 20);
+            Weapons LongHorsebackBow = new Weapons("The Yumi Bow", 13, 15, true, 15);
+            Weapons StrongBlowBow = new Weapons("Manchu Bow", 35, 35, true, 20);
 
             SecretWeapons Excalibur = new SecretWeapons("Magical girl wand", 1000, false, 1);
             SecretWeapons Lostvayne = new SecretWeapons("Lostvayne", 500, false, 1);
