78ac2570a4ab11759f2cbd7a8a15bf040a08045e
lazyonly4u <tanushreeshrestha@gmail.com>
2025-09-15 13:30:55 +0200

    Update Player.cs

--- textgame4.0/Methods/Player.cs
+++ textgame4.0/Methods/Player.cs
@@ -1,12 +1,25 @@
-﻿using System;
+using Methods;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace textgame4._0.Methods
+namespace Methods
 {
-    internal class Player
+    public class Player
     {
+        public static int Health { get; set; } = 100;
+        public static int Coins { get; set; } = 10;
+        public static string Weapon { get; set; }
+        public static List<string> Items { get; set; } = new List<string>();
+        public static Room CurrentRoom { get; set; }
+        public static void DisplayStats()
+        {
+            Console.WriteLine($"Health: {Health}");
+            Console.WriteLine($"Coins: {Coins}");
+            Console.WriteLine($"Weapon: {Weapon}");
+            Console.WriteLine("Items: " + (Items.Count > 0 ? string.Join(", ", Items) : "None"));
+        }
     }
 }
