fc7620c929249993410df7cac7be73b7e6077f0f
lazyonly4u <tanushreeshrestha@gmail.com>
2025-09-15 13:30:02 +0200

    Update Intro.cs

--- textgame4.0/Methods/Intro.cs
+++ textgame4.0/Methods/Intro.cs
@@ -1,12 +1,38 @@
-﻿using System;
+using Methods;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace textgame4._0.Methods
+namespace Methods
 {
-    internal class Intro
+    public class intro
     {
+        public static void DisplayIntro()
+        {
+            Console.ReadLine();
+            Console.WriteLine(" .oooooo..o oooo   o8o                                                 .oooooo..o     .              .o8                           .oo.           .oooooo..o     .                .o88o.  .o88o. \r\nd8P'    `Y8 `888   `\"'                                                d8P'    `Y8   .o8             \"888                         .88' `8.        d8P'    `Y8   .o8                888 `\"  888 `\" \r\nY88bo.       888  oooo  ooo. .oo.  .oo.    .ooooo.   .oooo.o          Y88bo.      .o888oo  .oooo.    888oooo.   .oooo.o          88.  .8'        Y88bo.      .o888oo oooo  oooo  o888oo  o888oo  \r\n `\"Y8888o.   888  `888  `888P\"Y88bP\"Y88b  d88' `88b d88(  \"8           `\"Y8888o.    888   `P  )88b   d88' `88b d88(  \"8          `88.8P           `\"Y8888o.    888   `888  `888   888     888    \r\n     `\"Y88b  888   888   888   888   888  888ooo888 `\"Y88b.                `\"Y88b   888    .oP\"888   888   888 `\"Y88b.            d888[.8'            `\"Y88b   888    888   888   888     888    \r\noo     .d8P  888   888   888   888   888  888    .o o.  )88b .o.      oo     .d8P   888 . d8(  888   888   888 o.  )88b .o.      88' `88.        oo     .d8P   888 .  888   888   888     888    \r\n8\"\"88888P'  o888o o888o o888o o888o o888o `Y8bod8P' 8\"\"888P' Y8P      8\"\"88888P'    \"888\" `Y888\"\"8o  `Y8bod8P' 8\"\"888P' Y8P      `bodP'`88.      8\"\"88888P'    \"888\"  `V88V\"V8P' o888o   o888o   \r\n                                                              '                                                          '                                                                       \r\n                                                                                                                                                                                                 \r\n                                                                                                                                                                                                 ");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Congracts truck-kun ran you over and now you have regressed to the game world of Slimes, Stabs, & Stuff. Your mission win and try not to die");
+            Console.ReadLine();
+            Console.WriteLine("which you will");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+
+            Console.WriteLine("You look up and see a door with a sign saying : Hello sacrafice #584 ");
+            System.Threading.Thread.Sleep(2000);
+            Console.Clear();
+
+            Console.WriteLine("You blink and when you open your eyes the sign says: Hello adventurerer there are 7 levels(heyyy booo).");
+            Console.WriteLine("After each level you will unlock a new one. You will always have a choice of going back a level to grind or find more loot(don't worry ill be with you every step of the way).");
+            Console.WriteLine("Though you can choose to fight a monster of run but picking run during a battle will have consiquences, so pick at your own expense (*mentally grabs your hand*)");
+            Console.WriteLine("You can also choose to go to the shop to buy items and you can also choose to open your inventory to see what you have(and with each step you WILL have my input).");
+            Console.WriteLine("May the Great Winged Lion have mercy on you (ong)");
+            Console.ReadLine();
+            Console.Clear();
+        }
     }
 }
