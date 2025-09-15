3803a5ede33f47ac0a389eb4c442a2b6cbb1684e
lazyonly4u <tanushreeshrestha@gmail.com>
2025-09-15 13:31:50 +0200

    Update Secret Weapon.cs

--- textgame4.0/Methods/Secret Weapon.cs
+++ textgame4.0/Methods/Secret Weapon.cs
@@ -1,12 +1,131 @@
-﻿using System;
+using Methods;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace textgame4._0.Methods
+namespace Methods
 {
-    internal class Secret_Weapon
+    public class SecretWeapons
     {
+        public string Name { get; set; }
+        public int Damage { get; set; }
+        public bool IsRanged { get; set; }
+        public int UsesLeft { get; set; }
+
+        public SecretWeapons(string Name, int Damage, bool IsRanged, int usesLeft)
+        {
+            this.Name = Name;
+            this.Damage = Damage;
+            this.IsRanged = IsRanged;
+            UsesLeft = usesLeft;
+        }
+        public bool IsUsable()
+        {
+            return UsesLeft > 0;
+        }
+        public void Use()
+        {
+            if (UsesLeft > 0)
+            {
+                UsesLeft--;
+            }
+            else
+            {
+                Console.WriteLine($"The secret weapon {Name} has no uses left and cannot be used.");
+            }
+        }
+    }
+    public class secretitems
+    {
+        private static int wrongAttempts = 0;
+        public static void DisplaySecretItems()
+        {
+            Console.WriteLine("Welcome to the Secret Items Shop! Please enter the code: ");
+            string code = Console.ReadLine().ToLower();
+            if (code == "magi")
+            {
+                Console.WriteLine("You have entered Magical girl wand damage: 1000, do you wish to add into your inventory? (y or n):");
+                if (Console.ReadLine().ToLower() == "y")
+                {
+                    if (!Player.Items.Contains("Magical girl wand"))
+                    {
+                        Player.Items.Add("Magical girl wand");
+                        Console.WriteLine("Magical girl wand has been added to your inventory(CLOCK ITTTT)!");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("You already have Magicial girl wand in your inventory(ok girlie stop being greedy).");
+                        Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Maybe next time(huh?)!");
+                    Console.ReadLine();
+                }
+                wrongAttempts = 0; // reset on success
+            }
+            else if (code == "av4")
+            {
+                Console.WriteLine("You have entered Avatar powers damage: 880, do you wish to add into your inventory? (y or n):");
+                if (Console.ReadLine().ToLower() == "y")
+                {
+                    if (!Player.Items.Contains("Avatar Powers"))
+                    {
+                        Player.Items.Add("Avatar Powers");
+                        Console.WriteLine("Avatar Powers has been added to your inventory(CLOCK ITTTT)!");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("You already have Avatar Powers in your inventory(ok girlie stop being greedy).");
+                        Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Maybe next time(huh?)!");
+                    Console.ReadLine();
+                }
+                wrongAttempts = 0; // reset on success
+            }
+            else if (code == "l0st")
+            {
+                Console.WriteLine("You have entered Lostvayne (seven deadly sins) damage: 500, do you wish to add into your inventory? (y or n):");
+                if (Console.ReadLine().ToLower() == "y")
+                {
+                    if (!Player.Items.Contains("Lostvayne"))
+                    {
+                        Player.Items.Add("Lostvayne");
+                        Console.WriteLine("Lostvayne has been added to your inventory(CLOCK ITTTT)!");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("You already have Lostvayne in your inventory(ok girlie stop being greedy).");
+                        Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Maybe next time(huh?)!");
+                    Console.ReadLine();
+                }
+                wrongAttempts = 0; // reset on success
+            }
+            else
+            {
+                wrongAttempts++;
+                int penalty = wrongAttempts == 1 ? 1 : (wrongAttempts - 1) * 5;
+                Console.WriteLine($"Invalid code. Punishment: -{penalty} coins(LMAO YOU THOUGHTTT).");
+                Player.Coins -= penalty;
+                Console.WriteLine($"You now have {Player.Coins} coins(BROKEEEE).");
+                Console.WriteLine("Press Enter to continue...");
+                Console.ReadLine();
+            }
+        }
     }
 }
