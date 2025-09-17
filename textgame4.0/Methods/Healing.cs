0cd2a0c4fb19e397e0d28a2d1239328402548e6e
lazyonly4u <tanushreeshrestha@gmail.com>
2025-09-17 21:07:19 +0200

    Update Healing.cs

--- textgame4.0/Methods/Healing.cs
+++ textgame4.0/Methods/Healing.cs
@@ -7,11 +7,19 @@ using System.Threading.Tasks;
 
 namespace Methods
 {
+    // Represents a healing item (like a potion, food, etc.)
     public class Healing
     {
+        // Name of the healing item (e.g., "Health Potion")
         public string Name { get; set; }
+
+        // Cost of the healing item (in coins or some currency)
         public int Cost { get; set; }
+
+        // Amount of health this item restores when used
         public int Heal { get; set; }
+
+        // Constructor to initialize a healing item with its name, cost, and healing power
         public Healing(string Name, int Cost, int Heal)
         {
             this.Name = Name;
